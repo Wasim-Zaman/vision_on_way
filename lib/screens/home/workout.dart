172365307a2fcb128016d4852bf112d2813eeb41
Faysal-Zaman <faysalzuman00@gmail.com>
2022-09-09 15:45:01 +0500

    This is third commit

--- lib/screens/home/workout.dart
+++ lib/screens/home/workout.dart
@@ -27,197 +27,199 @@ class Workout extends StatelessWidget {
         backgroundColor: Colors.white,
         body: SafeArea(
           child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                SizedBox(
-                  height: 20.0,
-                  width: 100.0,
-                  // child: Divider(
-                  //   color: Colors.black,
-                  // ),
-                ),
-                InkWell(
-                  splashColor: Colors.purple[200],
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => MyAudioPlayer(),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  SizedBox(
+                    height: 20.0,
+                    width: 100.0,
+                    // child: Divider(
+                    //   color: Colors.black,
+                    // ),
+                  ),
+                  InkWell(
+                    splashColor: Colors.purple[200],
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => MyAudioPlayer(),
+                        ),
+                      );
+                    },
+                    child: Card(
+                      color: Color.fromARGB(255, 249, 238, 253),
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10.0,
+                        horizontal: 15.0,
                       ),
-                    );
-                  },
-                  child: Card(
-                    color: Color.fromARGB(255, 249, 238, 253),
-                    margin: EdgeInsets.symmetric(
-                      vertical: 10.0,
-                      horizontal: 15.0,
-                    ),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.album,
-                        size: 65,
-                        color: Colors.purple[500],
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.album,
+                          size: 65,
+                          color: Colors.purple[500],
+                        ),
+                        title: Text('SQUATS',
+                            style: TextStyle(
+                              fontSize: 25.0,
+                              color: Colors.purple[300],
+                            )),
+                        subtitle: Text(
+                            'No. of sets: 5 Sets \nCaloriesBurn:30 cal \nDuration: 5 mins',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              color: Colors.grey[700],
+                            )),
                       ),
-                      title: Text('SQUATS',
-                          style: TextStyle(
-                            fontSize: 25.0,
-                            color: Colors.purple[300],
-                          )),
-                      subtitle: Text(
-                          'No. of sets: 5 Sets \nCaloriesBurn:30 cal \nDuration: 5 mins',
-                          style: TextStyle(
-                            fontSize: 18.0,
-                            color: Colors.grey[700],
-                          )),
                     ),
                   ),
-                ),
-                InkWell(
-                  splashColor: Colors.purple[200],
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => MyAudioPlayer(),
+                  InkWell(
+                    splashColor: Colors.purple[200],
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => MyAudioPlayer(),
+                        ),
+                      );
+                    },
+                    child: Card(
+                      color: Color.fromARGB(255, 249, 238, 253),
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10.0,
+                        horizontal: 15.0,
                       ),
-                    );
-                  },
-                  child: Card(
-                    color: Color.fromARGB(255, 249, 238, 253),
-                    margin: EdgeInsets.symmetric(
-                      vertical: 10.0,
-                      horizontal: 15.0,
-                    ),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.album,
-                        size: 65,
-                        color: Colors.purple[500],
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.album,
+                          size: 65,
+                          color: Colors.purple[500],
+                        ),
+                        title: Text(' YOGA',
+                            style: TextStyle(
+                              fontSize: 25.0,
+                              color: Colors.purple[300],
+                            )),
+                        subtitle: Text(
+                            'No. of sets: 5 Sets \nCaloriesBurn:30 cal \nDuration: 5 mins',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              color: Colors.grey[700],
+                            )),
                       ),
-                      title: Text(' YOGA',
-                          style: TextStyle(
-                            fontSize: 25.0,
-                            color: Colors.purple[300],
-                          )),
-                      subtitle: Text(
-                          'No. of sets: 5 Sets \nCaloriesBurn:30 cal \nDuration: 5 mins',
-                          style: TextStyle(
-                            fontSize: 18.0,
-                            color: Colors.grey[700],
-                          )),
-                    ),
-                  ),
-                ),
-                InkWell(
-                  splashColor: Colors.purple[200],
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => MyAudioPlayer(),
-                      ),
-                    );
-                  },
-                  child: Card(
-                    color: Color.fromARGB(255, 249, 238, 253),
-                    margin: EdgeInsets.symmetric(
-                      vertical: 10.0,
-                      horizontal: 15.0,
-                    ),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.album,
-                        size: 65,
-                        color: Colors.purple[500],
-                      ),
-                      title: Text('PUSH UPS',
-                          style: TextStyle(
-                            fontSize: 25.0,
-                            color: Colors.purple[300],
-                          )),
-                      subtitle: Text(
-                          'No. of sets: 5 Sets \nCaloriesBurn:30 cal \nDuration: 5 mins',
-                          style: TextStyle(
-                            fontSize: 18.0,
-                            color: Colors.grey[700],
-                          )),
                     ),
                   ),
-                ),
-                InkWell(
-                  splashColor: Colors.purple[200],
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => MyAudioPlayer(),
+                  InkWell(
+                    splashColor: Colors.purple[200],
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => MyAudioPlayer(),
+                        ),
+                      );
+                    },
+                    child: Card(
+                      color: Color.fromARGB(255, 249, 238, 253),
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10.0,
+                        horizontal: 15.0,
                       ),
-                    );
-                  },
-                  child: Card(
-                    color: Color.fromARGB(255, 249, 238, 253),
-                    margin: EdgeInsets.symmetric(
-                      vertical: 10.0,
-                      horizontal: 15.0,
-                    ),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.album,
-                        size: 65,
-                        color: Colors.purple[500],
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.album,
+                          size: 65,
+                          color: Colors.purple[500],
+                        ),
+                        title: Text('PUSH UPS',
+                            style: TextStyle(
+                              fontSize: 25.0,
+                              color: Colors.purple[300],
+                            )),
+                        subtitle: Text(
+                            'No. of sets: 5 Sets \nCaloriesBurn:30 cal \nDuration: 5 mins',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              color: Colors.grey[700],
+                            )),
                       ),
-                      title: Text('STRETCHING',
-                          style: TextStyle(
-                            fontSize: 25.0,
-                            color: Colors.purple[300],
-                          )),
-                      subtitle: Text(
-                          'No. of sets: 5 Sets \nCaloriesBurn:30 cal \nDuration: 5 mins',
-                          style: TextStyle(
-                            fontSize: 18.0,
-                            color: Colors.grey[700],
-                          )),
-                    ),
-                  ),
-                ),
-                InkWell(
-                  splashColor: Colors.purple[200],
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => MyAudioPlayer(),
-                      ),
-                    );
-                  },
-                  child: Card(
-                    color: Color.fromARGB(255, 249, 238, 253),
-                    margin: EdgeInsets.symmetric(
-                      vertical: 10.0,
-                      horizontal: 15.0,
-                    ),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.album,
-                        size: 65,
-                        color: Colors.purple[500],
-                      ),
-                      title: Text('ZUMBA',
-                          style: TextStyle(
-                            fontSize: 25.0,
-                            color: Colors.purple[300],
-                          )),
-                      subtitle: Text(
-                          'No. of sets: 5 Sets \nCaloriesBurn:30 cal \nDuration: 5 mins',
-                          style: TextStyle(
-                            fontSize: 18.0,
-                            color: Colors.grey[700],
-                          )),
                     ),
                   ),
-                ),
-              ],
+                  InkWell(
+                    splashColor: Colors.purple[200],
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => MyAudioPlayer(),
+                        ),
+                      );
+                    },
+                    child: Card(
+                      color: Color.fromARGB(255, 249, 238, 253),
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10.0,
+                        horizontal: 15.0,
+                      ),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.album,
+                          size: 65,
+                          color: Colors.purple[500],
+                        ),
+                        title: Text('STRETCHING',
+                            style: TextStyle(
+                              fontSize: 25.0,
+                              color: Colors.purple[300],
+                            )),
+                        subtitle: Text(
+                            'No. of sets: 5 Sets \nCaloriesBurn:30 cal \nDuration: 5 mins',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              color: Colors.grey[700],
+                            )),
+                      ),
+                    ),
+                  ),
+                  InkWell(
+                    splashColor: Colors.purple[200],
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => MyAudioPlayer(),
+                        ),
+                      );
+                    },
+                    child: Card(
+                      color: Color.fromARGB(255, 249, 238, 253),
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10.0,
+                        horizontal: 15.0,
+                      ),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.album,
+                          size: 65,
+                          color: Colors.purple[500],
+                        ),
+                        title: Text('ZUMBA',
+                            style: TextStyle(
+                              fontSize: 25.0,
+                              color: Colors.purple[300],
+                            )),
+                        subtitle: Text(
+                            'No. of sets: 5 Sets \nCaloriesBurn:30 cal \nDuration: 5 mins',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              color: Colors.grey[700],
+                            )),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
